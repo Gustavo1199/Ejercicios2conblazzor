b8ccabb389b4403257579ea288bbf6277e49de7e
Your Name <you@example.com>
2021-01-31 17:59:20 -0400

    Primer Comit

---
+++ TAREA2/Pages/consumir.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace TAREA2.Pages
+{
+    public class consumir
+    {
+        public List<Result> traerDato()
+        {
+            WebClient cliente = new WebClient();
+            var api = "https://randomuser.me/api/?results=1";
+            var datos = cliente.DownloadString(api);
+            var resultado = JsonConvert.DeserializeObject<Root>(datos);
+
+            return resultado.results;
+        }
+
+
+    }
+}
